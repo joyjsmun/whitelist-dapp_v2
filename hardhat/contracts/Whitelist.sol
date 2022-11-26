c271de3e83c4e592f4780643f27d1392686dea5c
joyjsmun <joy.js.mun@gmail.com>
2022-11-26 21:37:57 +0900

    ADD: first commit with hardhat setting

--- hardhat/contracts/Whitelist.sol
+++ hardhat/contracts/Whitelist.sol
@@ -1,35 +1,27 @@
 //SPDX-License-Identifier: Unlicense
 pragma solidity ^0.8.0;
 
-
 contract Whitelist {
+    //max number of whitelisted addresses allowed
+    uint public maxWhitelistedAddresses;
 
-    uint8 public maxWhitelistedAddresses;
-
-    // Create a mapping of whitelistedAddresses
-    // if an address is whitelisted, we would set it to true, it is false my default for all other addresses.
+    //Create a mapping of whitelistedAddresses
+    //it is false by default
     mapping(address => bool) public whitelistedAddresses;
+    
+    //it would be used to keep track of how many addresses have been whitelisted
+    uint public numAddressesWhitelisted;
 
-    // numAddressesWhitelisted would be used to keep track of how many addresses have been whitelisted
-    uint8 public numAddressesWhitelisted;
-
+    //setting the Max number of whitelisted addresses
     constructor(uint8 _maxWhitelistedAddresses) {
-        maxWhitelistedAddresses =  _maxWhitelistedAddresses;
+        maxWhitelistedAddresses = _maxWhitelistedAddresses;
     }
 
-    /**
-        addAddressToWhitelist - This function adds the address of the sender to the
-        whitelist
-     */
     function addAddressToWhitelist() public {
-        // check if the user has already been whitelisted
-        require(!whitelistedAddresses[msg.sender], "Sender has already been whitelisted");
-        // check if the numAddressesWhitelisted < maxWhitelistedAddresses, if not then throw an error.
+        require(!whitelistedAddresses[msg.sender],"Sender has already been whitelisted");
         require(numAddressesWhitelisted < maxWhitelistedAddresses, "More addresses cant be added, limit reached");
-        // Add the address which called the function to the whitelistedAddress array
         whitelistedAddresses[msg.sender] = true;
-        // Increase the number of whitelisted addresses
         numAddressesWhitelisted += 1;
     }
-
 }
+
--- hardhat/scripts/deploy.js
+++ hardhat/scripts/deploy.js
@@ -1,26 +1,27 @@
-const {ethers} = require("hardhat");
-const { Contract } = require("hardhat/internal/hardhat-network/stack-traces/model");
+const { ethers } = require("hardhat");
 
-async function main(){
+async function main() {
+  /*
+  A ContractFactory in ethers.js is an abstraction used to deploy new smart contracts,
+  so whitelistContract here is a factory for instances of our Whitelist contract.
+  */
+  const whitelistContract = await ethers.getContractFactory("Whitelist");
 
-  /*A ContractFactory in ethers.js is an abstraction used to deploy new smart contracts,
-  so whitelistContract here is a factory for instances of our Whitelist Contract. */
-  const WhitelistContact = await ethers.getContractFactory("Whitelist");
+  // here we deploy the contract
+  const deployedWhitelistContract = await whitelistContract.deploy(10);
+  // 10 is the Maximum number of whitelisted addresses allowed
 
-  //deploy contract here "deploy()"
-  //10 is the max number of whitelisted addresses allowed
-  const deployedWhitelistContract = await WhitelistContact.deploy(10);
+  // Wait for it to finish deploying
+  await deployedWhitelistContract.deployed();
 
-  //wait for it to finish deploying "deployed()"
-  await deployedWhitelistContract.deployed()
-  
-  console.log("Whitelist Contract Address:",deployedWhitelistContract.address);
+  // print the address of the deployed contract
+  console.log("Whitelist Contract Address:", deployedWhitelistContract.address);
 }
 
-//call the main function and catch if there is any error
+// Call the main function and catch if there is any error
 main()
   .then(() => process.exit(0))
   .catch((error) => {
-    console.log(error);
+    console.error(error);
     process.exit(1);
-  })
+  });
